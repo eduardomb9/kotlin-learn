17f141f7801c1f69f8681a47d35816e8731ac7a5
Eduardo Maranhão <maranhaoeduardo9@gmail.com>
2021-09-15 11:59:14 -0300

    finalizando parte de sintaxe

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,16 +7,24 @@ fun main() {
 
 //    testaValorConta(saldo)
 
-    for (i in 5 downTo 1 step 2) {
+    /*for (i in 5 downTo 1 step 2) {
         println(i)
-    }
+    }*/
 
-    println("while loop")
+    /*println("while loop")
 
     var count = 0
     while (count < 5) {
         println(count)
         count++
+    }*/
+
+    loop@ for (i in 0..9) {
+        println("i: $i")
+        for (j in 0..9) {
+            println("j: $j")
+            if (j == 3) break@loop
+        }
     }
 
     println("O valor do salto atual na conta $numeroConta é: $saldo")
