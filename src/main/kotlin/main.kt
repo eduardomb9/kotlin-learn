a966e447df8dc96a0719b4f6bcf8785108b8724d
Eduardo Maranhão <maranhaoeduardo9@gmail.com>
2021-09-15 11:54:04 -0300

    iniciando repositorio curso alura kotlin primeiros passos e OO

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,37 @@
+fun main() {
+    var saldo: Double = 0.0
+    val numeroConta: Int = 1000
+
+    saldo += 100.50
+    saldo -= 1000.50
+
+//    testaValorConta(saldo)
+
+    for (i in 5 downTo 1 step 2) {
+        println(i)
+    }
+
+    println("while loop")
+
+    var count = 0
+    while (count < 5) {
+        println(count)
+        count++
+    }
+
+    println("O valor do salto atual na conta $numeroConta é: $saldo")
+}
+
+private fun testaValorConta(saldo: Double) {
+    when {
+        saldo > 0 -> {
+            println("O saldo é positivo!")
+        }
+        saldo == 0.0 -> {
+            println("O saldo está zerado!")
+        }
+        else -> {
+            println("O saldo está negativo!!!")
+        }
+    }
+}
