100fdbf58675d2138405f89ddfb17a2725278824
Eduardo Maranhão <maranhaoeduardo9@gmail.com>
2021-09-17 13:32:24 -0300

    heranca/polimorfismo/interface - aula 4 fim aula, criacao da classe cliente para acessar sistemainterno

--- src/main/kotlin/TestaFuncionarioAdmin.kt
+++ src/main/kotlin/TestaFuncionarioAdmin.kt
@@ -2,8 +2,11 @@ fun testaFuncionarioAdmin() {
     val gerente = Gerente("Alex", "111.111.111-11", 2000.0, "111")
     val diretor = Diretor("Pedro", "222.222.222-22", 4000.0, "222", 6000.0)
 
+    val cliente = Cliente("Thiago", "333.333.333-33", "333")
+
     val sistemaInterno = SistemaInterno()
 
     sistemaInterno.entra("111", gerente)
     sistemaInterno.entra("111", diretor)
+//    sistemaInterno.entra("333", cliente)
 }
