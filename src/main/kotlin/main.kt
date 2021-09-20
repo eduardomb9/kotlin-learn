93aa124dd1d85b026b4efec287985d5644fe45fa
Eduardo Maranhão <maranhaoeduardo9@gmail.com>
2021-09-20 12:32:13 -0300

    kotlin-recursos-da-linguagem-com-pacotes-e-composicao - aula 3 variaveis globais e objects: utilizando object expressions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,4 @@
-import br.com.alura.bytebank.modelo.Cliente
-import br.com.alura.bytebank.modelo.Endereco
-import br.com.alura.bytebank.modelo.totalContas
+import br.com.alura.bytebank.modelo.*
 import br.com.alura.bytebank.modelo.novoCliente.Cliente as NovoCliente
 
 fun main() {
@@ -13,6 +11,15 @@ fun main() {
     val cliente = Cliente("Bob", "111.111.111-11", "1", Endereco(numero="100", bairro="Bairro 1"))
     println("Cliente: ${cliente.nome} bairro: ${cliente.endereco.bairro} Número: ${cliente.endereco.numero}")
     println("Total de contas criadas: $totalContas")
+
+    val c1 = object : Autenticavel {
+        val nome: String = "Pedro"
+        val cpf: String = "222.222.222-22"
+        val senha: String = "2"
+
+        override fun autentica(senha: String) = this.senha == senha
+    }
+    println("Cliente object expression: ${c1.nome} + ${c1.cpf} +  ${c1.senha} + ${c1.autentica("1")}")
 }
 
 fun testePacotesDiferentesMesmoNomeClasse() {
