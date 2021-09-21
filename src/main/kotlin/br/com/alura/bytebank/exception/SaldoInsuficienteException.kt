164c8baa86830d3819e3a8ddf8259b362c08e27c
Eduardo Maranhão <maranhaoeduardo9@gmail.com>
2021-09-21 13:13:31 -0300

    kotlin lidando com exceptions e referências nulas - aula 5 lidando com referencias nulas

---
+++ src/main/kotlin/br/com/alura/bytebank/exception/SaldoInsuficienteException.kt
@@ -0,0 +1,3 @@
+package br.com.alura.bytebank.exception
+
+class SaldoInsuficienteException(mensagem: String = "Saldo é insuficiente!") : Exception(mensagem)
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,6 @@
+import br.com.alura.bytebank.modelo.Endereco
 import br.com.alura.bytebank.teste.testeException
+import kotlin.math.log
 
 fun main() {
 //    executaTestesConta()
@@ -9,7 +11,21 @@ fun main() {
 //    testeObject()
 //    testeAny()
 //    testeEqualsHashCodeToString()
-    testeException()
+//    testeException()
+    testeNullPointerException()
 
+}
+
+fun testeNullPointerException() {
+    var enderecoNulo: Endereco? = Endereco(logradouro = "RUA B", complemento = "predio")
+    var logradouroNaoNulo: String? = enderecoNulo?.logradouro
+    println(logradouroNaoNulo)
+    println(enderecoNulo?.logradouro?.toString())
+    enderecoNulo?.let {
+        println(it.logradouro.toString())
+        // Elvis Operator ?: da aula
+        val complemento = it.complemento?.length ?: 0
+        println(complemento)
+    }
 }
 
