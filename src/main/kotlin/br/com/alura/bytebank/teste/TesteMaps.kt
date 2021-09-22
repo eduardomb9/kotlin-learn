894d4386895375db304eb2bc9c20a8a96124c301
Eduardo Maranhão <maranhaoeduardo9@gmail.com>
2021-09-22 16:40:07 -0300

    kotlin Collections: Set e Map - aula 4 explorando operacoes especificas do Map

--- src/main/kotlin/br/com/alura/bytebank/teste/TesteMaps.kt
+++ src/main/kotlin/br/com/alura/bytebank/teste/TesteMaps.kt
@@ -18,4 +18,37 @@ fun testeMaps() {
     println(pedidos)
     pedidos.remove(5)
     println(pedidos)
+
+    println(pedidos.getOrElse(6, defaultValue = {-1.0}))
+    // java.util.NoSuchElementException: Key 6 is missing in the map.
+//    pedidos.getValue(6)
+    println(pedidos.getOrDefault(6, -1.0))
+    pedidos.keys.forEach { i -> println(i) }
+    pedidos.values.forEach { i -> println(i) }
+    println(pedidos.filter { (chave, valor) -> chave % 2 == 0 && valor > 20.0 })
+    println(pedidos.filterValues { valor -> valor > 20.0 })
+    println(pedidos.filterKeys { chave -> chave % 2 == 0 })
+
+    println(pedidos + setOf(7 to 40.0, 8 to 10.0))
+    println(pedidos + (7 to 40.0))
+    println(pedidos - 3)
+
+    pedidos.putAll(setOf(7 to 40.0, 8 to 10.0))
+    println(pedidos)
+
+    pedidos += 6 to 20.0
+    println(pedidos)
+
+    pedidos -= setOf(7,8)
+    println(pedidos)
+
+    pedidos.keys.remove(3)
+    println(pedidos)
+
+    pedidos.values.remove(20.0)
+    println(pedidos)
+
+    pedidos.values.remove(20.0)
+    println(pedidos)
+
 }
