08d12a97cd03b304c50e366eb05ad57f5dbeedb8
Eduardo Maranhão <maranhaoeduardo9@gmail.com>
2021-09-24 10:35:38 -0300

    kotlin recursos do paradigma funcional: aula 3 explorando as high order functions

--- src/main/kotlin/br/com/alura/bytebank/modelo/SistemaInterno.kt
+++ src/main/kotlin/br/com/alura/bytebank/modelo/SistemaInterno.kt
@@ -3,11 +3,13 @@ package br.com.alura.bytebank.modelo
 import br.com.alura.bytebank.modelo.Autenticavel
 
 class SistemaInterno {
-    fun entra(senha: String, autenticavel: Autenticavel) {
+    fun entra(senha: String, autenticavel: Autenticavel, autenticado: () -> Unit = {}) {
         if (autenticavel.autentica(senha)) {
             println("Autenticado com sucesso!")
+            autenticado()
         } else {
             println("Falhou na autenticacao!")
         }
+
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,6 @@
-import br.com.alura.bytebank.teste.testaFuncaoAnonimaELambda
-import br.com.alura.bytebank.teste.testeInicializacaoVariaveisTipoFuncao
-import kotlin.reflect.KFunction
+import br.com.alura.bytebank.modelo.Autenticavel
+import br.com.alura.bytebank.modelo.Endereco
+import br.com.alura.bytebank.modelo.SistemaInterno
 
 fun main() {
 //    executaTestesConta()
@@ -22,8 +22,57 @@ fun main() {
 //    testeMaps()
 //    testaAssociateEGroupBy()
 
-    testeInicializacaoVariaveisTipoFuncao()
-    testaFuncaoAnonimaELambda()
+//    testeInicializacaoVariaveisTipoFuncao()
+//    testaFuncaoAnonimaELambda()
 
+    val testeFuncao: () -> Unit
+    Endereco().let {
+        it
+    }
+    "".let(::testeRecebeString)
+    1.let {
+        it
+    }
+
+    teste(1, {})
+
+    Endereco(complemento = "casa a", logradouro = "rua a").let { ("${it.logradouro} - ${it.complemento}").uppercase() }
+        .let(block = (::println))
+
+    val enderecos = listOf(
+        Endereco(complemento = "casa b"),
+        Endereco(),
+        Endereco(complemento = "Apartamento")
+    ).filter { it.complemento.isNotEmpty() }.let(::println))
+
+    implementandoHighOrderFunction()
 }
 
+fun implementandoHighOrderFunction() {
+    soma(2, 5) {
+        println(it)
+    }
+
+    val autenticavel = object : Autenticavel {
+        val senha = "111"
+        override fun autentica(senha: String): Boolean = this.senha == senha
+    }
+
+    SistemaInterno().entra("111", autenticavel) {
+        println("faz uma operacao que requer autenticacao!")
+    }
+}
+
+fun testeRecebeString(valor: String) {
+}
+
+//higher order function
+fun teste(teste: Int, bloco: () -> Unit) {
+    println(bloco)
+}
+
+fun soma(a: Int, b: Int, resultado: (Int) -> Unit) {
+    println("Executando funcao soma!")
+    resultado(a + b)
+    println("Soma executada com sucesso!")
+}
